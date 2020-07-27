318f6e4688cc06a479e2312ef9096564806e0603
Didier Berlioz <berliozd@gmail.com>
2020-07-27 23:42:56 +0200

    initial commi

---
+++ src/components/Foo/foo.js
@@ -0,0 +1,12 @@
+/* src/components/Foo/foo.js */
+
+import React, { useEffect } from 'react';
+
+const Foo = () => {
+
+  return (
+    <h1>Hello World JSX bababab</h1>
+  );
+}
+
+export default Foo;
