f9881ad42b167894b5ab7bcefe3ed4d829a9c109
Didier Berlioz <berliozd@gmail.com>
2020-07-29 23:38:27 +0200

    Adding a css file with a component

--- src/components/Foo/foo.js
+++ src/components/Foo/foo.js
@@ -1,12 +1,17 @@
-/* src/components/Foo/foo.js */
+import React from 'react';
 
-import React, { useEffect } from 'react';
+import classes from './foo.css';
+import Greeting from '../Greeting';
 
-const Foo = () => {
-
-  return (
-    <h1>Hello World JSX</h1>
-  );
+const Foo = props => {
+    return (
+    <div className={classes.root}>
+        <h1 className={classes.title}>Foo Component</h1>
+        <hr className={classes.spacer} />
+        <p className={classes.label}>A child component with propTypes &amp; CSS Modules:</p>
+        <Greeting name="Joe Bloggs" className={classes.title} />
+    </div>
+    );
 }
 
 export default Foo;
